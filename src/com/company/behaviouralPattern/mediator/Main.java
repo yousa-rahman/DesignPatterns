c1d54dfb90968d8eb31b67c79106466ecb7c78fc
yousa-rahman <yousa.rahman@gmail.com>
2021-10-24 21:14:02 +0530

    mediator

---
+++ src/com/company/behaviouralPattern/mediator/Main.java
@@ -0,0 +1,10 @@
+package com.company.behaviouralPattern.mediator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArticlesDialogBox dialog = new ArticlesDialogBox();
+
+        dialog.simulateUserInteraction();
+    }
+}
